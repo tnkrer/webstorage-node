1ff03a4a26e3a8c02b4487e35c732ed41b771653
John Su <git@tyncture.dev>
2019-08-11 11:35:44 +0100

    Export all required properties in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-function main() {
-}
+import { StorageBackend } from "./backend";
+import { StorageProxy } from "./proxy";
+import { Storage, StorageType } from "./types";
 
-main();
+export const sessionStorage: Storage = new Proxy(
+  new StorageBackend(StorageType.SessionStorage),
+  StorageProxy,
+);
 
+export const localStorage: Storage = new Proxy(
+  new StorageBackend(StorageType.LocalStorage),
+  StorageProxy,
+);
+
+export const WebStorageNode = {
+  sessionStorage,
+  localStorage,
+};
+
+export default WebStorageNode;
