dda760618473bb7d8af24b83dda8618ec5d0aceb
John Su <git@tyncture.dev>
2019-08-11 10:17:55 +0100

    Update interfaces to reflect new approach using ES6 proxies

--- src/types.ts
+++ src/types.ts
@@ -9,19 +9,15 @@ export interface Storage {
   setItem: (key: string, value: string) => void;
   removeItem: (key: string) => void;
   clear: () => void;
-  // Custom handling of getter/setters operators
-  get: (name: any) => string;
-  set: (name, value) => string;
 }
 
 // Library API
-export interface WebStorage {
-  readonly sessionStorage: Storage;
-  readonly localStorage: Storage;
-}
-
-// Internal backend API
 export enum StorageType {
   SessionStorage = "sessionStorage",
   LocalStorage = "localStorage"
+}
+
+export interface WebStorage {
+  readonly sessionStorage: Storage;
+  readonly localStorage: Storage;
 }
