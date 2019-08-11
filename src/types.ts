74742fef8f05959bbe406bdd1889c97c3180b3ce
John Su <git@tyncture.dev>
2019-08-11 07:40:15 +0100

    Create interfaces for implementation based on Web Storage standards

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+// Storage interface implementation
+// Based on the HTML Living Standard specification
+// https://html.spec.whatwg.org/multipage/webstorage.html
+
+export interface Storage {
+  readonly length: number;
+  key: (index: number) => string;
+  getItem: (key: string) => string;
+  setItem: (key: string, value: string) => void;
+  removeItem: (key: string) => void;
+  clear: () => void;
+  // Custom handling of getter/setters operators
+  get: (name: any) => string;
+  set: (name, value) => string;
+}
+
+// Library API
+export interface WebStorage {
+  readonly sessionStorage: Storage;
+  readonly localStorage: Storage;
+}
+
+// Internal backend API
+export enum StorageType {
+  SessionStorage = "sessionStorage",
+  LocalStorage = "localStorage"
+}
+
+export interface StorageBackend extends Storage {
+  // Statelessly initialise DB on each operation
+  prepare: () => void;
+}
+
